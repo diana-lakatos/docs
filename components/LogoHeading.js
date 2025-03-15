ff84a242b0c3cbeff6ea0655537986f5271ff057
Diana Lakatos <suada@t-online.hu>
2025-03-15 23:46:38 +0100

    Documentation edits made through Mintlify web editor

--- components/LogoHeading.js
+++ components/LogoHeading.js
@@ -1,11 +1,10 @@
 const LogoHeading = ({ src, alt, text }) => {
-    return (
-      <h2 style={{ display: "flex", alignItems: "center", gap: "10px", alignItems: "center" }}>
-        <img src={src} alt={alt} style={{ width: "24px", height: "24px" }} />
-        {text}
-      </h2>
-    );
-  };
-  
-  export default LogoHeading;
-  
+  return (
+    <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+      <img src={src} alt={alt} style={{ width: "24px", height: "24px" }} />
+      <h2 style={{ margin: 0 }}>{text}</h2>
+    </div>
+  );
+};
+
+export default LogoHeading;
